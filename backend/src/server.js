1777f166fb66e7b6ed61284199e409498b3e78fb
Naresh Jogu <kumarn29346@gmail.com>
2026-08-13 11:44:12 +0530

    feat: connect mongoDB database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,16 @@
-const http = require("http");
+require("dotenv").config();
+
 const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
-const server = http.createServer(app);
+const startServer = async () => {
+  await connectDB();
+};
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
+
+startServer();
