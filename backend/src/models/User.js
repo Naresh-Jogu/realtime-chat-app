af7e52228e8dd359544b5735ff6bac0750e021c6
Naresh Jogu <kumarn29346@gmail.com>
2026-08-13 12:22:38 +0530

    feat: add user model

---
+++ backend/src/models/User.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      trype: String,
+      required: true,
+      minLength: 6,
+      select: false,
+    },
+
+    avatar: {
+      type: String,
+      default: "",
+    },
+
+    isOnline: {
+      type: Boolean,
+      default: false,
+    },
+
+    lastSeen: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Hashing the  password before saving the user
+
+userSchema.pre("save", async function (next) {
+  // Don't hash the password again if it was't modified
+
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+// Compare entred password with hashed password
+
+userSchema.methods.comparePassword = async function (entredPassword) {
+  return bcrypt.compare(entredPassword, this.password);
+};
+
+const user = mongoose.model("User", userSchema);
+
+module.exports = user;
