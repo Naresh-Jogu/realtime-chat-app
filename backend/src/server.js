cc4319715b8aba6cf3659c55a5c3cfa02a9e3f8f
Naresh Jogu <kumarn29346@gmail.com>
2026-08-14 16:55:59 +0530

    feat: initialize socket.io  server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = require("./app");
 const connectDB = require("./config/db");
 
@@ -7,10 +10,27 @@ const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   await connectDB();
-};
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
-});
+  const server = http.createServer(app);
+
+  const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log(`Socket connected: ${socket.id}`);
+
+    socket.on("disconnect", () => {
+      console.log(`Socket Connected: ${socket.id}`);
+    });
+  });
+
+  server.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
+};
 
 startServer();
